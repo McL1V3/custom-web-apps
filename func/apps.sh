ff3772629a688615121737523eed4df6321367ec
Cesar Andres Salvatierra Espinoza <mclive.case@gmail.com>
2020-07-24 17:49:13 -0500

    adding

--- func/apps.sh
+++ func/apps.sh
@@ -121,7 +121,7 @@ wordpress_with_database(){
     break;
     fi
     done
-    echo "$(sudo cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)"
+    echo "cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1"
     return
     PASSWDDB=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
